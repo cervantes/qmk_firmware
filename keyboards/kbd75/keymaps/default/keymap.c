f59aa598a2256dcff5ca6f15c10081e15214375d
Carlos Cervantes <thecarlos@rodete-desktop-imager.corp.google.com>
2018-09-25 16:42:37 -0700

    Spacebar now activates the mouse layer with a hold, but still sends space on tap.

--- keyboards/kbd75/keymaps/default/keymap.c
+++ keyboards/kbd75/keymaps/default/keymap.c
@@ -3,6 +3,7 @@
 #define _______ KC_TRNS
 #define FN_ESC LT(1,KC_ESC)
 #define MS_TAB LT(2,KC_TAB)
+#define MS_SPC LT(2,KC_SPC)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -12,7 +13,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     MS_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,            KC_PGUP,
     KC_LCTL,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,                      KC_ENT,   KC_PGDN,
     KC_LSFT,  MO(1),    KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,            KC_UP,    KC_END,
-    KC_LCTL,  KC_LGUI,  KC_LALT,                      KC_SPC,   KC_SPC,   KC_SPC,                       KC_RALT,  MO(1),    KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT
+    KC_LCTL,  KC_LGUI,  KC_LALT,                      MS_SPC,   MS_SPC,   MS_SPC,                       KC_RALT,  MO(1),    KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT
   ),
 
   [1] = LAYOUT(
