a5e9f2cf2111ea514ca6c5e241dab9b7603e312d
Carlos Cervantes <thecarlos@rodete-desktop-imager.corp.google.com>
2018-10-24 13:45:39 -0700

    Added rgblight_disable/rgblight_enable to make it so the underglow is my caps lock indicator. If the underglow is on, caps lock is disabled. Underglow off, caps lock enabled.

--- keyboards/kbd75/keymaps/cervantes/keymap.c
+++ keyboards/kbd75/keymaps/cervantes/keymap.c
@@ -40,7 +40,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 void led_set_user(uint8_t usb_led) {
     if (usb_led & (1 << USB_LED_CAPS_LOCK)) {
         DDRB |= (1 << 2); PORTB &= ~(1 << 2);
+        rgblight_disable();
     } else {
         DDRB &= ~(1 << 2); PORTB &= ~(1 << 2);
+        rgblight_enable();
     }
 }
