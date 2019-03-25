91ee57c413ac9ae9001764e76e2603f2680e81c9
Carlos Cervantes <thecarlos@google.com>
2019-03-25 15:52:45 -0700

    Changed LCTL to a function key and made the layer 1 of lalt to be ctl-alt in order to preserve my keyboard locking muscle memory.

--- keyboards/kbdfans/kbd75/keymaps/cervantes/keymap.c
+++ keyboards/kbdfans/kbd75/keymaps/cervantes/keymap.c
@@ -5,6 +5,7 @@
 #define MS_TAB LT(2,KC_TAB)
 #define MS_SPC LT(2,KC_SPC)
 #define CAPSCTL CTL_T(KC_CAPS)
+#define KC_LCA LCA(KC_NO)
 
 void matrix_init_user(void) {
 #ifdef RGBLIGHT_ENABLE
@@ -20,7 +21,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     MS_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,            KC_PGUP,
     CAPSCTL,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,                      KC_ENT,   KC_PGDN,
     KC_LSFT,  MO(1),    KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,            KC_UP,    KC_END,
-    KC_LCTL,  KC_LGUI,  KC_LALT,                      MS_SPC,   MS_SPC,   MS_SPC,                       KC_RALT,  MO(1),    KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT
+    MO(1),    KC_LGUI,  KC_LALT,                      MS_SPC,   MS_SPC,   MS_SPC,                       KC_RALT,  MO(1),    KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT
   ),
 
   [1] = LAYOUT(
@@ -29,7 +30,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______,  RGB_TOG,  RGB_MOD,  RGB_HUI,  RGB_HUD,  RGB_SAI,  RGB_SAD,  RGB_VAI,  RGB_VAD,  _______,  _______,  _______,  _______,  _______,            _______,
     KC_CAPS,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,                      _______,  _______,
     _______,  _______,  _______,  _______,  BL_DEC,   BL_TOGG,  BL_INC,   BL_STEP,  _______,  _______,  _______,  _______,  _______,            KC_VOLU,  KC_MUTE,
-    _______,  _______,  _______,                      _______,  _______,  _______,                      _______,  _______,  _______,  _______,  KC_VOLD,  _______
+    _______,  _______,  KC_LCA,                       _______,  _______,  _______,                      _______,  _______,  _______,  _______,  KC_VOLD,  _______
   ),
 
   // Mouse layer
