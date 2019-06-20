103f64b52d8ae303d6f0b00436dd32e301bbffa0
Carlos Cervantes <thecarlos@google.com>
2019-06-20 09:23:45 -0700

    Left CTRL is now a function key. Left alt is now alt+ctrl on layer 1 to preserve muscle memory for screen locking.

--- keyboards/dztech/dz65rgb/keymaps/cervantes/keymap.c
+++ keyboards/dztech/dz65rgb/keymaps/cervantes/keymap.c
@@ -2,6 +2,7 @@
 
 #define _______ KC_TRNS
 #define CAPSCTL CTL_T(KC_CAPS)
+#define KC_LCA LCA(KC_NO)
 
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -10,13 +11,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLASH, KC_PGUP, \
       CAPSCTL,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,            KC_ENT,    KC_PGDN, \
       KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,            KC_UP,     KC_END, \
-      KC_LCTL,  KC_LGUI,  KC_LALT,                      KC_SPC,                       KC_RALT,  MO(1),    KC_RCTL,            KC_LEFT,  KC_DOWN,   KC_RIGHT),
+      MO(1),    KC_LGUI,  KC_LALT,                      KC_SPC,                       KC_RALT,  MO(1),    KC_RCTL,            KC_LEFT,  KC_DOWN,   KC_RIGHT),
   [1] = LAYOUT_65_ansi( /* FN */
-      KC_GESC,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_DEL,    KC_INS, \
+      KC_GRV,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_DEL,    KC_INS, \
       _______,  RGB_TOG,  RGB_MOD,  RGB_HUI,  RGB_HUD,  RGB_SAI,  RGB_SAD,  RGB_VAI,  RGB_VAD,  _______,  KC_PSCR,  KC_SLCK,  KC_PAUS,  RESET,     _______, \
       KC_CAPS,  RGB_SPI,  RGB_SPD,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            EEP_RST,   _______, \
       _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            KC_VOLU,   KC_MUTE, \
-      _______,  _______,  _______,                      _______,                      _______,  _______,  _______,            KC_MPRV,  KC_VOLD,   KC_MNXT),
+      _______,  _______,  KC_LCA,                       _______,                      _______,  _______,  _______,            KC_MPRV,  KC_VOLD,   KC_MNXT),
 };
 
 void rgb_matrix_indicators_user(void)
