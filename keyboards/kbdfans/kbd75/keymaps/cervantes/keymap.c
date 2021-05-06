83a6fb3e81a2876aa8905aa7d59f80be845c66e1
Carlos Cervantes <ccervantes@methanesat.org>
2021-05-06 09:41:09 -0700

    Make it Mac style.

--- keyboards/kbdfans/kbd75/keymaps/cervantes/keymap.c
+++ keyboards/kbdfans/kbd75/keymaps/cervantes/keymap.c
@@ -21,7 +21,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     MS_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,            KC_PGUP,
     CAPSCTL,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,                      KC_ENT,   KC_PGDN,
     KC_LSFT,  MO(1),    KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,            KC_UP,    KC_END,
-    MO(1),    KC_LGUI,  KC_LALT,                      MS_SPC,   MS_SPC,   MS_SPC,                       KC_RALT,  MO(1),    KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT
+    MO(1),    KC_LALT,  KC_LGUI,                      MS_SPC,   MS_SPC,   MS_SPC,                       KC_RALT,  MO(1),    KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT
   ),
 
   [1] = LAYOUT(
